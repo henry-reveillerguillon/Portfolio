ddb37bfddf05d08c7ea88891209ed6f22fccfb81
Henry <henry.rg86@gmail.com>
2021-02-06 18:41:42 +0100

    bugs

---
+++ portrait.php
@@ -0,0 +1,30 @@
+<div class="content" id="portrait">
+                        <h2>Photographie de portrait</h2>
+                          <div class="case-study-gallery">
+                              <div class="case-study2 study25">
+                                  <div class="case-study__overlay">
+                                      <h3 class="case-study__title">Alicia</h3>
+                                      <a target="_blank" class="case-study__link" href="https://henryreveillerguillon.wordpress.com/portfolio/alicia/">Voir l'album</a>
+                                  </div>
+                              </div>
+                              <div class="case-study2 study26">
+                                  <div class="case-study__overlay">
+                                      <h3 class="case-study__title">Léo</h3>
+                                      <a target="_blank" class="case-study__link" href="https://baptiste-houlle-medium.netlify.com/">Voir l'album</a>
+                                  </div>
+                              </div>
+                              <div class="case-study2 study27">
+                                <div class="case-study__overlay">
+                                  <h3 class="case-study__title">Henry</h3>
+                                  <a target="_blank" class="case-study__link" href="http://perg417z6-rwerdocko.fr/">Voir l'album</a>
+                                </div>
+                              </div>
+                              <div class="case-study2 study28">
+                                <div class="case-study__overlay">
+                                  <h3 class="case-study__title">Noryne</h3>
+                                  <a target="_blank" class="case-study__link" href="https://henryreveillerguillon.wordpress.com/portfolio/noryne/">Voir l'album</a>
+                                </div>
+                              </div>
+                        </div>
+                      </div>
+                        
---
+++ presentation.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Henry REVEILLER-GUILLON - Portfolio</title>
+  <link rel="stylesheet" href="assets/style.bundle.css">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  </head>
+  <html lang="fr"><head><style data-merge-styles="true"></style>
+    <script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js"></script><script async="" src="https://www.googletagmanager.com/gtag/js?id=UA-156062138-1"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="/assets/images/will.png">
+    </head>
+<div class="content" id="presentation">
+                        <h2>Presentation</h2>
+                       
+<div id="mybio" class="slide">
+               <div id="mybio-content">
+                <div class="row">
+                    <div class="col-12 col-lg-auto text-center align-items-center justify-content-center d-flex">
+                        <div id="mybio-photo"></div>
+                    </div>
+                    <div class="col-12 col-md px-3">
+                        <div id="mybio-text-wrapper" >
+                            <div class="mybio-text" is="cursor-hide">
+                                <p>
+                                  Je m'appelle Henry Reveiller-Guillon, je suis un étudiant en première année à l’IUT Bordeaux-Montaigne dans la formation Métiers du Multimédia et de l’Internet (MMI). Passionnée depuis toujours par les nouvelles technologies du multimédia. C'est pourquoi j'ai choisi cette formation, j'ai pu acquérir de nombreuses compétences dans le domaine du web design et du développement web avec la maîtrise de nombreux logiciels et de différents langages webs. Mes compétences englobent aussi la communication, la gestion de projet, la gestion de bases de données et de services sur réseaux.
+                                </p>
+                                <p>
+                                    <a class="i-link" href="https://www.linkedin.com/in/henry-reveiller-guillon-a8593719b" data-hover="focus"><u>Voir mon profil Linkedin</u></a>
+                                </p>
+                                <p>
+                                  <a class="i-link" href="https://www.behance.net/henryreveill" data-hover="focus"><u>Voir mon profil Behance</u></a>
+                              </p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    </body></html>
