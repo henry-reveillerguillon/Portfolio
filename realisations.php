57ad2fc972cb529c7255f63d770b99ef63bd8630
Henry <henry.rg86@gmail.com>
2021-02-19 02:53:22 +0100

    bug

---
+++ realisations.php
@@ -0,0 +1,66 @@
+<div class="content" id="realisations">
+    <h2>Réalisations</h2>
+    <div class="case-study-gallery">
+        <div class="case-study study1">
+            <div class="case-study__overlay">
+                <h3 class="case-study__title">JEU VIDEO SUR SCRATCH</h3>
+                <p DRACULA+></p>
+                <a target="_blank" class="case-study__link" href="https://www.behance.net/gallery/105331367/MMI-Projet-Scratch">Voir le
+                    site web</a>
+            </div>
+        </div>
+
+        <div class="case-study study2">
+            <div class="case-study__overlay">
+                <h3 class="case-study__title">REDISIGN COVER DE BIGFLO & OLI</h3>
+                <a target="_blank" class="case-study__link" href="https://example.com/">Voir le site web</a>
+            </div>
+        </div>
+
+        <div class="case-study study3">
+            <div class="case-study__overlay">
+                <h3 class="case-study__title">LENS SNAPCHAT</h3>
+                <a target="_blank" class="case-study__link" href="https://example.com/">Voir le site web</a>
+            </div>
+        </div>
+
+        <div class="case-study study4">
+            <div class="case-study__overlay">
+                <h3 class="case-study__title">REDESIGN DU MUSEE DE L'IMPRIMERIE</h3>
+                <a target="_blank" class="case-study__link" href="https://example.com/">Voir la page web</a>
+                <a target="_blank" class="case-study__link" href="https://example.com/">Voir le code source</a>
+            </div>
+
+        </div>
+        <div class="case-study study5">
+            <div class="case-study__overlay">
+                <h3 class="case-study__title">DESIGN DE JEU VIDEO</h3>
+                <a target="_blank" class="case-study__link" href="https://example.com/">Voir la page web</a>
+                <a target="_blank" class="case-study__link" href="https://example.com/">Voir le code source</a>
+            </div>
+        </div>
+
+        <div class="case-study study6">
+            <div class="case-study__overlay">
+                <h3 class="case-study__title">MOTION DESIGN</h3>
+                <a target="_blank" class="case-study__link" href="https://www.youtube.com/watch?v=a_lx-mt20SI">Voir la
+                    page web</a>
+            </div>
+        </div>
+
+        <div class="case-study study7">
+            <div class="case-study__overlay">
+                <h3 class="case-study__title">LES INTERMITTENTS DU SPECTABLE PENDANT LA COVID</h3>
+                <a target="_blank" class="case-study__link" href="/">Voir le site web</a>
+                <a target="_blank" class="case-study__link" href="https://example.com/">Voir le code source</a>
+            </div>
+        </div>
+
+        <div class="case-study study8">
+            <div class="case-study__overlay">
+                <h3 class="case-study__title">VERT CHEZ TOI - Episode pilote</h3>
+                <a target="_blank" class="case-study__link" href="/">Voir le projet</a>
+            </div>
+        </div>
+    </div>
+</div>
