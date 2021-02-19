91f34259a70366ae33709b3626604ad4a18e3510
Henry <henry.rg86@gmail.com>
2021-02-19 02:47:30 +0100

    cccc

---
+++ competences.php
@@ -0,0 +1,76 @@
+<div class="content" id="competences">
+    <h2>Compétences</h2>
+    <div class="content__competences1__boite">
+        <div class="content__competences1__boite__picto">
+            <i class="fab fa-trello"></i>
+        </div>
+        <div class="content__competences1__boite__texte">Trello</div>
+    </div>
+
+    <div class="content__competences1__boite">
+        <div class="content__competences1__boite__picto">
+            <i class="fab fa-wordpress"></i>
+        </div>
+        <div class="content__competences1__boite__texte">Wordpress</div>
+    </div>
+
+    <div class="content__competences1__boite">
+        <div class="content__competences1__boite__picto">
+            <i class="fab fa-discord"></i>
+        </div>
+        <div class="content__competences1__boite__texte">Discord</div>
+    </div>
+</div>
+
+<div class="content__competences1">
+    <div class="content__competences1__boite">
+        <div class="content__competences1__boite__picto">
+            <img src="images/ai.svg" alt="">
+        </div>
+        <div class="content__competences1__boite__texte">Illustrator</div>
+    </div>
+
+    <div class="content__competences1__boite">
+        <div class="content__competences1__boite__picto">
+            <img src="images/ps.svg" alt="">
+        </div>
+        <div class="content__competences1__boite__texte">Photoshop</div>
+    </div>
+
+    <div class="content__competences1__boite">
+        <div class="content__competences1__boite__picto">
+            <img src="images/ae.svg" alt="">
+        </div>
+        <div class="content__competences1__boite__texte">After Effects</div>
+    </div>
+
+    <div class="content__competences1__boite">
+        <div class="content__competences1__boite__picto">
+            <img src="images/pr.svg" alt="">
+        </div>
+        <div class="content__competences1__boite__texte">Premiere Pro</div>
+    </div>
+</div>
+</section>
+<section>
+    <div class="content" id="formations">
+        <h2>Formations</h2>
+        <div class="formations">
+            <div class="formations__image">
+                <img src="images/iut.jpg" alt="Photo iut Bordeaux Montaigne">
+            </div>
+            <div class="formations__texte">
+                <div class="formations__texte__titre">
+                    <p>DUT MMI (Métiers du Multimédia et de l'Internet)</p>
+                </div>
+                <div class="formations__texte__lieu">
+                    <p>IUT Bordeaux-Montaigne (2020/2022)</p>
+                </div>
+                <div class="formations__texte__paragraphe">
+                    <p>Apprentissage du <strong>web</strong>, du <strong>graphisme</strong> avec la suite adobe, audiovisuel
+                        et du motion design avec la suite adobe, communication, marketing, gestion de projets, ...
+                        Réalisations
+                        de maquettes et de sites web responsive, notions d’érgonomie UI/UX et d’accessibilité.</p>
+                </div>
+            </div>
+        </div>
